ef762ecc8ea3c183db55a13b5b449a914de015ba
LQY318 <53596411+LQY318@users.noreply.github.com>
2019-08-09 17:27:05 +0800

    Update README.py

--- C03/README.py
+++ C03/README.py
@@ -1,14 +1,11 @@
-(1) 收集数据:提供的文本文件。
+(1) 收集数据:提供的文本文件，在data/目录中
 (2) 准备数据:解析tab键分隔的数据行。
 (3) 分析数据:快速检查数据,确保正确地解析数据内容
 (4) 训练算法:createTree.py用于创建树结构
-            storageTree.py用于存储以及加载树结构
+            storageTree.py用于存储以及加载树结构，Tree.txt则是保存的树结构
             plotTree.py用于绘制树结构图
 (5) 测试算法:classifyTree.py构建分类器
 (6) 使用算法:main.py是用于测试/使用的主函数
-
-Tree.txt则是保存的树结构
-数据集在data/目录中
 
 存在的问题：
 1.plot_tree.py中的DepthTree函数为什么要进行深度的比较？
