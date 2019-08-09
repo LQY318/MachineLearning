e53a43cd4ec9a23fafb75b45c432280977bdc964
LQY318 <53596411+LQY318@users.noreply.github.com>
2019-08-09 09:38:25 +0800

    Update README.py

--- C03/README.py
+++ C03/README.py
@@ -4,6 +4,8 @@ plotTree.py用于绘制树结构图
 storageTree.py用于存储以及加载树结构
 Tree.txt则是保存的树结构
 main.py是用于测试的主函数，所用数据集在data/目录中
+
+
 存在的问题：
 1.plot_tree.py中的DepthTree函数为什么要进行深度的比较？
 2.classifyTree.py中classifier函数：未声明全局变量则返回pred_label时报错未定义pred_label，不知道为什么？
